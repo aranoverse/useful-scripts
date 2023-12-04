6755e20917f70b2fc1b5ad6a799310bb04c06d73
Arano <aranoverse@outlook.com>
2023-12-04 16:37:00 +0800

    feat: apt docker

---
+++ installation/apt_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install dependencies
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add current user to the docker group
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Print Docker and Docker Compose versions
+docker --version
+docker-compose --version
+
+# Output a message indicating successful installation
+echo "Docker and Docker Compose have been successfully installed."
+echo "Please restart your shell or run 'newgrp docker' to apply group changes."
